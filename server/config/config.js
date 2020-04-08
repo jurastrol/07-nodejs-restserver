f2f94e21779c340fe771390992567e6bea24e7e3
jurastrol <jurastrol@alumnos.unex.es>
2020-04-08 09:56:44 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://julufc6:hDNgr7ldeS0UmRoy@cluster0-jj1qq.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
